6d950502524344b0d21fdd204d6be753f958d0d4
Kevin-M254 <kmburu007@gmail.com>
2022-12-05 23:31:51 +0300

    function that returns a tuple with the length of a string and its first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def multiple_return(sentence):
+    """Returns the length of a string and its first char."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
