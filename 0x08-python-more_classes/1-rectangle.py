0fe65c586f71e639eb7f9ecc8ac5c55f454b91db
Kevin-M254 <kmburu007@gmail.com>
2023-01-04 12:03:15 +0300

    class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -40,7 +40,7 @@ class Rectangle:
     def height(self, value):
         """sets height attribute"""
         if not isinstance(value, int):
-            raise TypeError(value, int)
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
